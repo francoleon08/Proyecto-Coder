d323da82eb485a2f6a09ca22bb372bed57046958
Franco León <francoleon284@gmail.com>
2022-04-29 12:47:50 -0300

    commit footer grid y personalizacion

--- js/script.js
+++ js/script.js
@@ -5,10 +5,4 @@ $(window).scroll(function() {
     } else {
         $('.header').removeClass('header--scroll');
     }
-
-    if ($(window).scrollTop() > 100) {
-        $('.nav__item--index').addClass('.nav__item');
-    } else {
-        $('.nav__item--index').removeClass('.nav__item');
-    }
 })
