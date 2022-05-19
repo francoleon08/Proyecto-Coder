43d56458b6ce87571634f7b7230f1dacb0d26f51
Franco Leon <francoleon08@hotmail.com>
2022-05-19 18:19:44 -0300

    boton con js Productos enlace a Whatsapp

---
+++ js/boton-productos.js
@@ -0,0 +1,4 @@
+function redirigir(codigo) {
+    var x= 'https://api.whatsapp.com/send?phone=+5492915328868&text=Hola, Nececito mas informacion sobre el producto *' + codigo +'*';
+    window.open(x);
+}
