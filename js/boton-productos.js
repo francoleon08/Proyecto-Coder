497ccb66afe645f0327e3af6ed0edbfbff65a3d2
Franco Leon <francoleon08@hotmail.com>
2022-06-01 20:18:35 -0300

    contenido real en todas las secciones

--- js/boton-productos.js
+++ js/boton-productos.js
@@ -1,4 +1,4 @@
 function redirigir(codigo) {
-    var x= 'https://api.whatsapp.com/send?phone=+5492915328868&text=Hola, Nececito mas informacion sobre el producto *' + codigo +'*';
+    var x= 'https://api.whatsapp.com/send?phone=+5492915328868&text=Hola, Nececito mas informacion sobre el cuadro *' + codigo +'*';
     window.open(x);
 }
